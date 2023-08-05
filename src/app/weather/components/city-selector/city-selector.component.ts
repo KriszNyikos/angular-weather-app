316c1b3c62f6c46582125b1424455f0f0bd952e4
KriszNyikos <krisz.nyikos@gmail.com>
2023-08-05 13:52:04 +0200

    update input field UI

--- src/app/weather/components/city-selector/city-selector.component.ts
+++ src/app/weather/components/city-selector/city-selector.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Subscription, debounceTime, distinctUntilChanged } from 'rxjs';
 import { CitySelectorVM } from 'src/app/model/city-selector.component.vm';
@@ -6,32 +13,40 @@ import { CitySelectorVM } from 'src/app/model/city-selector.component.vm';
 @Component({
   selector: 'app-city-selector',
   templateUrl: './city-selector.component.html',
-  styleUrls: ['./city-selector.component.scss']
+  styleUrls: ['./city-selector.component.scss'],
 })
-export class CitySelectorComponent implements OnInit, OnDestroy{
+export class CitySelectorComponent implements OnInit, OnDestroy {
+  @Input() cityList: CitySelectorVM | null = null;
 
-  @Input() cityList: CitySelectorVM | null = null
-
-  public cityNameControl = new FormControl()
-  private _citySubscripion?: Subscription
-  @Output() cityNameChange = new EventEmitter<string>()
-  @Output() cityNameSelected = new EventEmitter<string>()
+  public cityNameControl = new FormControl();
+  private _citySubscripion?: Subscription;
+  @Output() cityNameChange = new EventEmitter<string>();
+  @Output() cityNameSelected = new EventEmitter<string>();
 
   ngOnInit(): void {
-     this._citySubscripion = this.cityNameControl.valueChanges.pipe(debounceTime(500),distinctUntilChanged()).subscribe((query: string) => {
-        this.cityNameChange.emit(query)
-      })
+    this._citySubscripion = this.cityNameControl.valueChanges
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe((query: string | null) => {
+        if (query === null || query === '') {
+          return;
+        } else {
+          this.cityNameChange.emit(query);
+        }
+      });
   }
-  
+
   ngOnDestroy(): void {
-      this._citySubscripion?.unsubscribe()
+    this._citySubscripion?.unsubscribe();
   }
 
-  onClickCityNameHandler(coordinates: string){
-    this.cityNameSelected.emit(coordinates)
+  onClickCityNameHandler(coordinates: string) {
+    this.cityNameControl.reset();
+    this.cityNameSelected.emit(coordinates);
   }
 
-  onEnterCityNameHandler(){
-    this.cityNameSelected.emit(this.cityNameControl.value)
+  onEnterCityNameHandler() {
+    this.cityNameSelected.emit(this.cityNameControl.value);
+    this.cityNameControl.reset();
+    this.cityList = null;
   }
 }
